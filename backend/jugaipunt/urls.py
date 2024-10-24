c456bb5de750eace2a85c9ea787d013f81ac7579
oriol barrachina crosas <152913457+oriol-Barra@users.noreply.github.com>
2024-10-24 14:28:37 +0200

    Update urls.py

--- backend/jugaipunt/urls.py
+++ backend/jugaipunt/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.crear_jugador, name='crear_jugador'),  # Ruta per a crear el jugador
     path('', views.login_view, name='login'), #Ruta per a la comprovació del login
+    path('', views.login_view, name='logout'), #Ruta per a la comprovació del login
+
+    
 ]
