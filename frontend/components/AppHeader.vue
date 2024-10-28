2359bb3d9a0294f6a8c6dfb416e07fae90325d50
josepsitjar <josepsitjar@gmail.com>
2024-10-29 00:45:47 +0100

    sidebar

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -25,40 +25,61 @@
       </nav>
       <div class="flex space-x-4">
         <template v-if="isAuthenticated">
-          <nuxt-link to="/profile">
-            <button class="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-700">
-              Perfil
-            </button>
-          </nuxt-link>
-          <nuxt-link to="/estadistiques">
-            <button class="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-700">
-              Estadístiques
-            </button>
-          </nuxt-link>
-          <!--Button crear lliga-->
-          <nuxt-link to="/crearlliga" >
-            <button v-if="isAdmin" class="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-700">
-              Crear lliga
-            </button>
-            <button v-if="!isAdmin" class="bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-700" disabled>
-              Crear lliga
-            </button>
-          </nuxt-link>
-          <!--Button resultat partides-->
-          <nuxt-link to="/resultatspartides">
-            <button v-if="isAdmin" class="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-700">
-              Resultats partides
-            </button>
-            <button v-if="!isAdmin" class="bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-700" disabled>
-              Resultats partides
-            </button>
-          </nuxt-link>
-          <button
-            class="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-700"
-            @click="logout"
-          >
-            Logout
-          </button>
+          <!--Botó Menú-->
+          <UButton v-if="isAdmin" label="Menú" @click="menuIsOpen = true" />
+          <!--Contingut del panell-->
+          <USlideover v-model="menuIsOpen" prevent-close>
+            <UCard class="flex flex-col flex-1" :ui="{ body: { base: 'flex-1' }, ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
+              <template #header>
+                <div class="flex items-center justify-between">
+                  <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
+                    Opcions de la plataforma
+                  </h3>
+                  <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="menuIsOpen = false" />
+                </div>
+              </template>
+
+              <Placeholder class="h-full" />
+
+              <div class="flex flex-col gap-2">
+                <nuxt-link to="/profile">
+                  <button class="w-full bg-green-500 text-white py-2 px-4 rounded hover:bg-green-700">
+                    Perfil
+                  </button>
+                </nuxt-link>
+                <nuxt-link to="/estadistiques">
+                  <button class="w-full bg-green-500 text-white py-2 px-4 rounded hover:bg-green-700">
+                    Estadístiques
+                  </button>
+                </nuxt-link>
+                <!--Button crear lliga-->
+                <nuxt-link to="/crearlliga" >
+                  <button v-if="isAdmin" class="w-full bg-green-500 text-white py-2 px-4 rounded hover:bg-green-700">
+                    Crear lliga
+                  </button>
+                  <button v-if="!isAdmin" class="w-full bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-700" disabled>
+                    Crear lliga
+                  </button>
+                </nuxt-link>
+                <!--Button resultat partides-->
+                <nuxt-link to="/resultatspartides">
+                  <button v-if="isAdmin" class="w-full bg-green-500 text-white py-2 px-4 rounded hover:bg-green-700">
+                    Resultats partides
+                  </button>
+                  <button v-if="!isAdmin" class="w-full bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-700" disabled>
+                    Resultats partides
+                  </button>
+                </nuxt-link>
+                <button
+                  class="w-full bg-red-500 text-white py-2 px-4 rounded hover:bg-red-700"
+                  @click="logout"
+                >
+                  Logout
+                </button>
+              </div>
+            </UCard>
+          </USlideover>
+
         </template>
         <template v-else>
           <nuxt-link to="/register">
@@ -71,6 +92,7 @@
               Iniciar Sessió
             </button>
           </nuxt-link>
+
         </template>
       </div>
     </div>
@@ -85,7 +107,8 @@ export default {
   data () {
     return {
       isAuthenticated: false, // Estat per verificar si l'usuari està autenticat
-      isAdmin: true           // Estar per verificar si l'usuari és admin
+      isAdmin: true,           // Estar per verificar si l'usuari és admin
+      menuIsOpen: false,
     }
   },
   mounted () {
