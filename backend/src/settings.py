7af5905359f4b895d803527dbdb61561184c8664
oriol barrachina crosas <152913457+oriol-Barra@users.noreply.github.com>
2024-10-28 17:55:32 +0100

    Update settings.py

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'src.wsgi.application'
 """
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': 'jugaipunt',
         'USER': 'grup2',
         'PASSWORD': 'grup2',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': '5432', 
     }
 }
