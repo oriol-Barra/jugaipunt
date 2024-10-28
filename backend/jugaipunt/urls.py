a78656171c77bc605d6619f5bbd861310c34879d
oriol barrachina crosas <152913457+oriol-Barra@users.noreply.github.com>
2024-10-28 18:47:23 +0100

    Update urls.py

--- backend/jugaipunt/urls.py
+++ backend/jugaipunt/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-    path('', views.crear_jugador, name='crear_jugador'),  # Ruta per a crear el jugador
-    path('', views.login_view, name='login'), #Ruta per a la comprovació del login
-    path('', views.login_view, name='logout'), #Ruta per al logout
+    path('jugador/', views.crear_jugador, name='crear_jugador'),  # Ruta per a crear el jugador
+    path('login/', views.login_view, name='login'),  # Ruta per a la comprovació del login
+    path('logout/', views.logout_view, name='logout'),  # Ruta per a la comprovació del login
 ]
