284b6590eed98e7ca2ca35d518b8a1edbf191f4a
oriolbc_ioc <oriol.b.c@hotmail.com>
2024-11-04 21:50:22 +0100

    tot

--- backend/jugaipunt/urls.py
+++ backend/jugaipunt/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('jugador', views.crear_jugador, name='crear_jugador/'),  # Ruta per a crear el jugador
     path('login', views.login_view, name='login/'),  # Ruta per a la comprovació del login
     path('logout', views.logout_view, name='logout/'),  # Ruta per a la comprovació del login
-    path('noutorneig', views.creatorneig_view, name='creartorneig/'),  # Ruta per a la creació d'un nou torneig
+   # path('noutorneig', views.creatorneig_view, name='creartorneig/'),  # Ruta per a la creació d'un nou torneig
     path('torneig/afegir_jugadors', views.afegirJugadors_view, name='afegirjugador/'),  # Ruta per a la creació d'un nou torneig
 
 
